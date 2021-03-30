925b66e85c077ab149b39cec90808f5cd2e17a5f
Aylton Almeida <ayltonalmeida2@gmail.com>
2021-03-30 19:50:08 -0300

    exchange[~]

--- Instrumentos/Codigos/Q2/exchange.py
+++ Instrumentos/Codigos/Q2/exchange.py
@@ -1,5 +1,14 @@
 from stackapi import StackAPI
+from src.models.Question import Question
+from datetime import datetime
 
 exchange = StackAPI('stackoverflow')
-comments = exchange.fetch('questions')
-print(comments)
+exchange.page_size = 100
+exchange.max_pages = 10
+raw_questions = exchange.fetch('questions', tagged='material-ui',
+                               fromdate=datetime(2020, 1, 1), todate=datetime(2020, 12, 31))
+
+questions = [Question(question) for question in raw_questions['items']]
+
+print(
+    '{}/{}'.format(len([question for question in questions if question.is_answered]), len(questions)))
