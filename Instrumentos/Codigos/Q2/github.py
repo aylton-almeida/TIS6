65482554d76ecce56f7747e029241b943ae12191
Aylton Almeida <ayltonalmeida2@gmail.com>
2021-04-14 21:37:50 -0300

    issue3

--- Instrumentos/Codigos/Q2/github.py
+++ Instrumentos/Codigos/Q2/github.py
@@ -54,52 +54,53 @@ def get_issue_ratio():
             temp_issues_list = []
 
             owner, name = repo.name_with_owner.split('/')
-            repo_csv = '{}_{}.csv'.format(owner, name)
-            CsvUtils.create_header_file(
-                repo_csv, ['index', 'createdAt', 'closedAt', 'cursor'])
+            repo_csv = '{}.csv'.format(name)
+            # CsvUtils.create_header_file(
+            #     repo_csv, ['index', 'createdAt', 'closedAt', 'cursor'])
 
             has_next_page = True
-            while has_next_page:
-                try:
-                    print('Fetching cursor: {}'.format(graphql.cursor))
+            # while has_next_page:
+            #     try:
+            #         print('Fetching cursor: {}'.format(graphql.cursor))
 
-                    # get query
-                    owner, name = repo.name_with_owner.split('/')
-                    query = graphql.get_issues_query(owner, name)
+            #         # get query
+            #         owner, name = repo.name_with_owner.split('/')
+            #         query = graphql.get_issues_query(owner, name)
 
-                    # fetch prs
-                    issues, has_next_page = graphql.get_issues_data(
-                        query, token.get_token())
+            #         # fetch prs
+            #         issues, has_next_page = graphql.get_issues_data(
+            #             query, token.get_token())
 
-                    # sleep sometime
-                    temp_issues_list += issues
-                    if len(temp_issues_list) > 0 and len(temp_issues_list) % 1000 == 0:
-                        time.sleep(60)
+            #         # sleep sometime
+            #         temp_issues_list += issues
+            #         if len(temp_issues_list) > 0 and len(temp_issues_list) % 1000 == 0:
+            #             time.sleep(60)
 
-                    # add issues to list and save
-                    parsed_issues = [{'createdAt': item.get('node').get(
-                        'createdAt'), 'closedAt':  item.get('node').get('closedAt'), 'cursor': graphql.cursor} for item in issues]
-                    issue_list += parsed_issues
-                    CsvUtils.save_list_to_csv(
-                        parsed_issues, repo_csv, mode='a', header=False)
+            #         # add issues to list and save
+            #         parsed_issues = [{'createdAt': item.get('node').get(
+            #             'createdAt'), 'closedAt':  item.get('node').get('closedAt'), 'cursor': graphql.cursor} for item in issues]
+            #         issue_list += parsed_issues
+            #         CsvUtils.save_list_to_csv(
+            #             parsed_issues, repo_csv, mode='a', header=False)
 
-                except Exception as err:
-                    time.sleep(600)
-                    token.next_token()
+            #     except Exception as err:
+            #         time.sleep(600)
+            #         token.next_token()
 
-            time.sleep(180)
+            # time.sleep(180)
 
             # read issues csv
-            # issues = CsvUtils.read_issues_from_csv(repo_csv)
+            issues = CsvUtils.read_issues_from_csv(
+                'issues/{}'.format(repo_csv))
 
-            # # calculate issues and save
-            # repo.calculate_issue_median(issues)
+            # calculate issues and save
+            repo.calculate_issue_median(issues)
 
-            # CsvUtils.save_list_to_csv(
-            #     [repo.__dict__], 'repos_with_issues.csv', mode='a', header=False)
+            CsvUtils.save_list_to_csv(
+                [repo.__dict__], 'repos_with_issues.csv', mode='a', header=False)
 
             # mv issues to folder
-            shutil.move(repo_csv, 'issues')
+            # shutil.move(repo_csv, 'issues')
 
             bar.update(index)
 
