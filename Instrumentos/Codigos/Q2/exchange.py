7087de0b54237e2d2affd9197d36c27e8294f8bc
Aylton Almeida <ayltonalmeida2@gmail.com>
2021-04-17 12:46:39 -0300

    .

--- Instrumentos/Codigos/Q2/exchange.py
+++ Instrumentos/Codigos/Q2/exchange.py
@@ -1,5 +1,4 @@
 import time
-import progressbar
 from src.models.Repo import Repo
 from src import CliArgs, CsvUtils, Exchange
 from dotenv import load_dotenv
@@ -7,21 +6,20 @@ from dotenv import load_dotenv
 # Load env file
 load_dotenv()
 
-# flush progress bar
-progressbar.streams.flush()
-
 
 def get_questions():
 
     # parse arguments
-    args = CliArgs.get_args(initialrepo=('Initial repo index', 0), file=(
-        'File in witch to save the results', 'repos_with_questions.csv'))
+    args = CliArgs.get_args(initialrepo=('Initial repo index', 0), out=(
+        'File in where to save the results', 'repos_with_questions.csv'), input=(
+        'File from where to read the repos', 'final_ui_repos.csv'))
 
     initial_repo = int(args.initialrepo)
-    file = args.file
+    out = args.out
+    input = args.input
 
     # read repos csv
-    repo_list = CsvUtils.read_repos_from_csv('final_ui_repos.csv')
+    repo_list = CsvUtils.read_repos_from_csv(input)
 
     trimmed_repos = repo_list[initial_repo:]
 
@@ -29,31 +27,29 @@ def get_questions():
 
     repo_list: list[Repo] = []
 
-    # extremely necessary progress bar for better user experience
-    with progressbar.ProgressBar(max_value=len(trimmed_repos), redirect_stdout=True) as bar:
-        for index in range(len(trimmed_repos)):
-            repo = trimmed_repos[index]
+    for index in range(len(trimmed_repos)):
+        repo = trimmed_repos[index]
 
-            questions = None
+        questions = None
 
-            time.sleep(180)
+        time.sleep(1)
 
-            while questions is None:
-                try:
-                    questions = Exchange.get_questions_by_tag(repo.get_name())
-                except:
-                    time.sleep(600)
+        print('Fetching repo {}...'.format(repo.name_with_owner))
 
-            repo.set_answered_questions(questions)
+        while questions is None:
+            try:
+                questions = Exchange.get_questions_by_tag(repo.get_name())
+            except:
+                time.sleep(180)
 
-            if repo.answered_questions:
-                CsvUtils.save_repos_to_csv(
-                    [repo], file, mode='a', header=False)
-            else:
-                CsvUtils.save_repos_to_csv(
-                    [repo], 'no_questions_found.csv', mode='a', header=False)
+        repo.set_answered_questions(questions)
 
-            bar.update(index)
+        if repo.answered_questions:
+            CsvUtils.save_list_to_csv(
+                [repo.__dict__], out, mode='a', header=False)
+        else:
+            CsvUtils.save_list_to_csv(
+                [repo.__dict__], 'no_questions_found.csv', mode='a', header=False)
 
 
 if __name__ == '__main__':
--- Instrumentos/Codigos/Q2/src/Exchange.py
+++ Instrumentos/Codigos/Q2/src/Exchange.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import os
 from stackapi import StackAPI
-from TimeCountdown import sleep
+from src.TimeCountdown import sleep
 
 from src.models.Question import Question
 
@@ -14,8 +14,8 @@ exchange.page_size = 100
 exchange.max_pages = 10
 
 
-def get_questions_by_tag(filter: str):
-    raw_questions = exchange.fetch('questions', filter=filter,
+def get_questions_by_tag(tag: str):
+    raw_questions = exchange.fetch('questions', tag=tag,
                                    fromdate=datetime(2020, 1, 1), todate=datetime(2020, 12, 31))
 
     quota_remaining = int(raw_questions['quota_remaining'])
