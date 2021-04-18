f2ef53068e1e4e9aa569af8acf1f3932724a8b08
Aylton Almeida <ayltonalmeida2@gmail.com>
2021-04-18 16:48:12 -0300

    node

--- Instrumentos/Codigos/Q4/src/utils/Node.py
+++ Instrumentos/Codigos/Q4/src/utils/Node.py
@@ -5,7 +5,7 @@ import pandas as pd
 def search_package(name: str):
     os.system('npm search {} > result.csv'.format(name))
 
-    df = pd.read_csv('result.csv', delimiter='|')
+    df = pd.read_csv('result.csv', delimiter='|', error_bad_lines=False)
     df.columns = df.columns.str.strip()
     pkg_name = next(
         (row['NAME'] for index, row in df.iterrows() if name in row['NAME']), None)
