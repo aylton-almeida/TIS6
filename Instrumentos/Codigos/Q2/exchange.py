84a2dfc049b2876a813a6ca8c4f36f3451abb9e7
Pedro Andrade <pedroandradef@outlook.com>
2021-04-19 22:55:18 -0300

    test

--- Instrumentos/Codigos/Q2/exchange.py
+++ Instrumentos/Codigos/Q2/exchange.py
@@ -10,17 +10,18 @@ data = read_csv(csvName)
 names = data['name'].tolist()
 
 for name in names:
+    print(name)
     exchange = StackAPI('stackoverflow')
     exchange.page_size = 100
     exchange.max_pages = 10
-    raw_questions = exchange.fetch('questions', tagged='material-ui',
+    raw_questions = exchange.fetch('questions', tagged=name,
                                    fromdate=datetime(2020, 1, 1), todate=datetime(2020, 12, 31))
 
     questions = [Question(question) for question in raw_questions['items']]
 
     print(
         '{}/{}'.format(len([question for question in questions if question.is_answered]), len(questions)))
-    data={'name': name, 'is_answered':len([question for question in questions if question.is_answered]) }
-    if(len([question for question in questions if question.is_answered]) > 0 & len(questions) > 0):
-        data['average'] = (len([question for question in questions if question.is_answered]) / len(questions))
+    data={'name': name, 'is_answered':len([question for question in questions if question.is_answered]), 'questions': len(questions) }
+    if(data.get('is_answered') > 0 & data.get('questions') > 0):
+        data['average'] = (data.get('is_answered') / data.get('questions'))
     DataFrame([data]).to_csv('stackoverflow.csv', mode='a', header=False, index=False)
