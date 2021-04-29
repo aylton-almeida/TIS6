d86caac1d7ee169f1f49df8b539b684f2b0dc01e
Aylton Almeida <ayltonalmeida2@gmail.com>
2021-04-29 19:26:32 -0300

    repo2

--- Instrumentos/Codigos/bottom10/2-ej2-react-ui-components/src/App.js
+++ Instrumentos/Codigos/bottom10/2-ej2-react-ui-components/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, FormControl } from "styled-bootstrap-components";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
         <div className="container">
           <form action="">
             <h2 className="title">Sign in here</h2>
-            <Button>
+            <ButtonComponent>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 aria-labelledby="fr6gzqks1vhcbf6piwt5uqrcolk3oq6"
@@ -31,17 +31,17 @@ function App() {
                 <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"></path>
               </svg>
               Sign in with Google
-            </Button>
+            </ButtonComponent>
             <hr className="divider" />
             <div className="input-container">
               <label className="field-label">Username or Email Address</label>
-              <FormControl />
+              <input className="e-input" type="text" />
             </div>
             <div className="input-container">
               <label className="field-label">Password</label>
-              <FormControl />
+              <input className="e-input" type="text" />
             </div>
-            <Button>Sign in</Button>
+            <ButtonComponent>Sign in</ButtonComponent>
           </form>
         </div>
       </main>
